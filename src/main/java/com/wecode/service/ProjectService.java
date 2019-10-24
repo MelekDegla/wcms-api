03bbebf402c93aad94c7d9008bfc2ab071ce3d87
mejrihaifa <55098057+mejrihaifa@users.noreply.github.com>
2019-10-24 19:12:13 +0100

    Crud Project (#1)

---
+++ src/main/java/com/wecode/service/ProjectService.java
@@ -0,0 +1,26 @@
+package com.wecode.service;
+
+import com.wecode.entity.Project;
+import com.wecode.repository.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class ProjectService {
+    @Autowired
+    private ProjectRepository projectRepository   ;
+
+    public List<Project> findAll(){return projectRepository.findAll();}
+    public Project update(Project p){
+        return projectRepository.save(p);
+
+    }
+    public Project save( Project p){ return projectRepository.save(p);    }
+
+    public Project findById(Long id){ return projectRepository.findById(id).get();}
+
+    public void deleteById(Long id){  projectRepository.deleteById(id);}
+
+
+}
