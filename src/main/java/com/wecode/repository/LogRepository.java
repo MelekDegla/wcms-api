86b20861ad13ec64bf2f5eed1fd7a77ef4bb66ab
Tatay <chokla95@gmail.com>
2019-11-17 22:06:04 +0100

    log

---
+++ src/main/java/com/wecode/repository/LogRepository.java
@@ -0,0 +1,11 @@
+package com.wecode.repository;
+
+
+import com.wecode.entity.Log;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LogRepository extends JpaRepository<Log, Long> {
+
+}
